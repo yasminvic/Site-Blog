341bdaf41ae810b0165987db4f63956abbdc6a18
Yasmin <yasminalvesdesouza27@gmail.com>
2023-02-26 18:42:49 -0300

    Layout NavBar

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 from models.Post import posts
 
@@ -8,5 +8,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 app.run(debug = True, host = '0.0.0.0', port = 5000)
